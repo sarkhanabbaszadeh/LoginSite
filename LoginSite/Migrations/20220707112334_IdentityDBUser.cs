702cf38e65322ad149df13b69662b322656780f9
Abbaszadeh <Abbaszadeh@ABBASZADEH-PC>
2022-07-07 23:03:53 +0400

    Add project files.

---
+++ LoginSite/Migrations/20220707112334_IdentityDBUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LoginSite.Migrations
+{
+    public partial class IdentityDBUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
